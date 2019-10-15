1b72c90fdd40031a93d440d40b4cb06741d67f19
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-14 22:31:38 -0400

    Updates

---
+++ precache-manifest.2dda16c55f6703b07fcc52c7c09b442b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d68ad4cc5bfa5500a17a83bca8329e3",
+    "url": "/cydportfolio/index.html"
+  },
+  {
+    "revision": "b3e33c4c4c1aa021010b",
+    "url": "/cydportfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ba27662ef1250424590a",
+    "url": "/cydportfolio/static/js/2.6f9a2b42.chunk.js"
+  },
+  {
+    "revision": "b3e33c4c4c1aa021010b",
+    "url": "/cydportfolio/static/js/main.c7cfca25.chunk.js"
+  },
+  {
+    "revision": "814c659b0a9f610add7f",
+    "url": "/cydportfolio/static/js/runtime-main.912af726.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cydportfolio/static/media/logo.25bf045c.svg"
+  }
+]);
