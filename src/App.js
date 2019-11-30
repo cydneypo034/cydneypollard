db6b50a4f54cd6f67b480a388dc98af47da935d7
Cydney Pollard <sincerelycydney087@gmail.com>
2019-11-30 13:18:09 -0500

    added fine arts folder

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import about from './components/about/about';
 import graphicarts from './components/graphicarts/graphicarts';
 import skills from './components/skills/skills';
 import contact from './components/contact/contact';
+import finearts from './components/finearts/finearts.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/portfolio" component={portfolio} />
         <Route path="/about" component={about} />
         <Route path="/graphicarts" component={graphicarts} />
+        <Route path="/finearts" component={finearts} />
         <Route path="/skills" component={skills} />
         <Route path="/contact" component={contact} />
       </Switch>
