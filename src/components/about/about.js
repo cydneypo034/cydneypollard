9809c536faa3c3186b39a0517efd7a9242b8aedf
Cydney Pollard <sincerelycydney087@gmail.com>
2020-01-04 14:41:18 -0500

    added new image for my about page, and editted about section. changed navigation bar to a functional component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import './about.css';
+import cydney from '../images/cydneyprofilepic.jpg';
 
 const About = () => (
     <div>
-                <div className="container">
-                    
-                
-
-                <p className="para-center">
+        <div className="container">
+                <p className="about-center">
+                <img src={cydney} alt="girl-with-yellow-and-red-african-shirt" className="profilepic-image" />
                 <h2 className="text-center">here's my story</h2>
-                I love making people's ideas into tangible things that help them reach more people
-                and increase their impact. Since I was a child, I have been an artist, but would get discouraged in pursuing it as a career path
-                because of the fear of being a "starving artist" and I allowed comparison between myself and other classically trained artist to disrupt that pursue.
-                I started working as a custodian at a theme park and realized that I was working in a job that I only took because I was afraid to pursue after what I 
-                truly wanted. I began work to pursue a creative career and saw graphic design as an enjoyable passion. I also learned how to code and in the process, broke 
-                the boundary I placed on myself that I wasn't able to learn anything that had to do with science.  With being a graphic designer, I 
-                like being able to take people's ideas and make them into a reality. Now that I know how to code as a software developer, I can take 
-                those ideas and take them a step further. 
+                I am a software engineer with an eye for Front-End Web Development. 
+                I have a passion for creating and have used my creative capabilities for service in different ways.
+                My love for drawing began to form when I was in elementary school and since then, I wanted to find my place in the world
+                as a creative person. It took many years of working as an graphic designer, focusing on designing brand identities and working as
+                a visual artist in digital illustration and painting. I still love and am involved in these forms of creativity, but I searched for another
+                way I could be creative and have a satisfying career. Hence, the start of my journey as a web developer began in July of 2019.
+                Through front-end web development, I have grown to love the process of designing layouts for web applications and 
+                it challenges me to think of how to structure my coding projects, what things to consider when it comes to the
+                memory and time complexity of data, and how to figure out what real world problem I am solving.
                 </p>
                 <br />
-                
-                </div>
-            </div>
+        </div>
+    </div>
 )
 
 export default About;
--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {Navbar, Nav, Button} from 'react-bootstrap';
 
-class NavigationBar extends React.Component {
-    render() {
-        return (
-          <Navbar  expand="lg">
+const NavigationBar = () => (
+  <Navbar expand="lg">
           <Navbar.Brand href="#/" className="para-center">Home</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="justify-content-end">
@@ -14,12 +12,12 @@ class NavigationBar extends React.Component {
               <Nav.Link href="#/skills" className="para-center">Skills</Nav.Link>
               <Nav.Link href="#/contact" className="para-center">Contact</Nav.Link>
               
-              <Button  variant="outline-warning" href="https://pdfhost.io/v/@EocChKm3_CydneyPollardCVpdf.pdf" className="button-text">Resume</Button>
+              <Button  variant="outline-warning" 
+              href="https://pdfhost.io/v/CnDkmHE@_cydneypollardresume102019pdf.pdf" 
+              className="button-text">Resume</Button>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        )
-    }
-}
+)
 
 export default NavigationBar;
